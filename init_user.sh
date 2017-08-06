8c340e128010dc91d1d778cb2408ec58156af983
Marcos Canán <mcanan@gmail.com>
2017-08-06 20:53:08 -0300

    xfce terminal script

--- init_user.sh
+++ init_user.sh
@@ -1,37 +1,41 @@
 #!/bin/bash 
 
 # Backup dotfiles
-rm -rf $HOME/backup_dotfiles
-mkdir -p $HOME/backup_dotfiles
-mv -f $HOME/.vimrc $HOME/backup_dotfiles/
-mv -f $HOME/.vim $HOME/backup_dotfiles/
-mv -f $HOME/.bash_profile $HOME/backup_dotfiles/
-mv -f $HOME/.bash_aliases $HOME/backup_dotfiles/
-mv -f $HOME/.bash_exports $HOME/backup_dotfiles/
-mv -f $HOME/.dotfiles $HOME/backup_dotfiles/
-mv -rf $HOME/vim $HOME/backup_dotfiles/
+rm -rf ~/backup_dotfiles
+mkdir -p ~/backup_dotfiles
+mv -f ~/.vimrc ~/backup_dotfiles/
+mv -f ~/.vim ~/backup_dotfiles/
+mv -f ~/.bash_profile ~/backup_dotfiles/
+mv -f ~/.bash_aliases ~/backup_dotfiles/
+mv -f ~/.bash_exports ~/backup_dotfiles/
+mv -f ~/.dotfiles ~/backup_dotfiles/
+mv -rf ~/vim ~/backup_dotfiles/
+mv -f ~/.config/xfce4/terminal/terminalrc ~/backup_dotfiles/
 
 # Clone dotfiles
-mkdir -p $HOME/bin
-mkdir -p $HOME/opt
-git clone --recursive https://github.com/mcanan/dotfiles.git $HOME/.dotfiles
+mkdir -p ~/bin
+mkdir -p ~/opt
+git clone --recursive https://github.com/mcanan/dotfiles.git ~/.dotfiles
 
 # Vim
-git clone https://github.com/vim/vim.git $HOME/vim
-cd $HOME/vim/src
-./configure --enable-pythoninterp --enable-python3interp --with-features=huge --prefix=$HOME/opt/vim
+git clone https://github.com/vim/vim.git ~/vim
+cd ~/vim/src
+./configure --enable-pythoninterp --enable-python3interp --with-features=huge --prefix=~/opt/vim
 make clean && make && make install
-ln -s $HOME/opt/vim/bin/vim $HOME/bin/vim
-ln -s $HOME/.dotfiles/.vim/vimrc $HOME/.vimrc
-ln -s $HOME/.dotfiles/.vim/ $HOME/.vim
+ln -s ~/opt/vim/bin/vim ~/bin/vim
+ln -s ~/.dotfiles/.vim/vimrc ~/.vimrc
+ln -s ~/.dotfiles/.vim/ ~/.vim
 
 # Bash
-ln -s $HOME/.dotfiles/bash_profile $HOME/.bash_profile
-ln -s $HOME/.dotfiles/bash_aliases $HOME/.bash_aliases
-ln -s $HOME/.dotfiles/bash_exports $HOME/.bash_exports
+ln -s ~/.dotfiles/bash_profile ~/.bash_profile
+ln -s ~/.dotfiles/bash_aliases ~/.bash_aliases
+ln -s ~/.dotfiles/bash_exports ~/.bash_exports
 
 # Tmux
-ln -s $HOME/.dotfiles/tmux.conf $HOME/.tmux.conf
+ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
 
 # Clean
-rm -rf $HOME/vim
+rm -rf ~/vim
+
+# Xfce terminal
+ln -s ~/.dotfiles/xfce_solarized_terminal ~/.config/xfce4/terminal/terminalrc
