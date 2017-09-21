a63a791c06835e02989df3880aa414c5c51aaeb0
Marcos Canán <mcanan@gmail.com>
2017-09-21 14:31:35 -0300

    bc

--- init.sh
+++ init.sh
@@ -11,3 +11,6 @@ sudo apt-get install -y git python-dev python3-dev python-pip python3-pip
 sudo apt-get remove -y vim vim-common
 sudo apt-get install -y build-essential libncurses5-dev ftp ctags
 sudo apt-get install -y tmux
+
+# Others
+sudo apt-get install -y bc
